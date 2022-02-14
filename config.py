e4318cd0b07e9fcedfd4896a09f082d1c39bb7f2
DwayneWard <neonyan15@gmail.com>
2022-02-14 17:39:56 +0300

    initial commit

---
+++ config.py
@@ -0,0 +1,5 @@
+class Config(object):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JSON_AS_ASCII = False
---
+++ views/genres.py
@@ -0,0 +1,3 @@
+from flask_restx import Namespace
+
+movie_ns = Namespace('movies')
