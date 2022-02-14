c0e690f5e9057d4d508abd812385f915789385be
DwayneWard <neonyan15@gmail.com>
2022-02-14 23:16:42 +0300

    Исправлена некорректная работа приложения. Добавлены view для отображения режиссеров и жанров. Добавлен файл README.

--- app.py
+++ app.py
@@ -1,49 +1,3 @@
-# основной файл приложения. здесь конфигурируется фласк, сервисы, SQLAlchemy и все остальное что требуется для приложения.
-# этот файл часто является точкой входа в приложение
-
-# Пример
-
-# from flask import Flask
-# from flask_restx import Api
-#
-# from config import Config
-# from models import Review, Book
-# from setup_db import db
-# from views.books import book_ns
-# from views.reviews import review_ns
-#
-# функция создания основного объекта app
-# def create_app(config_object):
-#     app = Flask(__name__)
-#     app.config.from_object(config_object)
-#     register_extensions(app)
-#     return app
-#
-#
-# функция подключения расширений (Flask-SQLAlchemy, Flask-RESTx, ...)
-# def register_extensions(app):
-#     db.init_app(app)
-#     api = Api(app)
-#     api.add_namespace(...)
-#     create_data(app, db)
-#
-#
-# функция
-# def create_data(app, db):
-#     with app.app_context():
-#         db.create_all()
-#
-#         создать несколько сущностей чтобы добавить их в БД
-#
-#         with db.session.begin():
-#             db.session.add_all(здесь список созданных объектов)
-#
-#
-# app = create_app(Config())
-# app.debug = True
-#
-# if __name__ == '__main__':
-#     app.run(host="localhost", port=10001, debug=True)
 from flask import Flask
 from flask_restx import Api
 
@@ -54,7 +8,7 @@ from views.genres import genre_ns
 from views.movies import movie_ns
 
 
-def create_app(config: Config) -> Flask:
+def create_app(config) -> Flask:
     """
     Функция реализует создание Flask приложения
     :param config: Конфигурация из файла config.py
@@ -81,7 +35,9 @@ def configure_app(application: Flask) -> None:
     api.add_namespace(genre_ns)
 
 
+app_config = Config()
+app = create_app(app_config)
+
+
 if __name__ == '__main__':
-    app_config = Config()
-    app = create_app(app_config)
     app.run()
--- config.py
+++ config.py
@@ -1,5 +1,7 @@
+from constants import DB_NAME
+
+
 class Config(object):
-    DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///movies.db'
+    SQLALCHEMY_DATABASE_URI = DB_NAME
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JSON_AS_ASCII = False
